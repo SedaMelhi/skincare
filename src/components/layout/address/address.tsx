04397578c56e2efaecbb27da84e4e000f7bbd33a
Rasul <overlord.ove@mail.ru>
2024-06-19 12:49:49 +0300

    cdek service

--- src/components/layout/address/address.tsx
+++ src/components/layout/address/address.tsx
@@ -1,17 +1,17 @@
-import { FC, useEffect, useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { CSSTransition } from 'react-transition-group';
-import { useRouter } from 'next/router';
-import { setIsAddressOpen } from '@/redux/addressSlice/addressSlice';
+import { FC, useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { CSSTransition } from "react-transition-group";
+import { useRouter } from "next/router";
+import { setIsAddressOpen } from "@/redux/addressSlice/addressSlice";
 
-import AsideHeader from './asideHeader/asideHeader';
-import CourierContent from './courierContent/courierContent';
-import PickupContent from './pickupContent/pickupContent';
-import YandexMap from './yandexMap/yandexMap';
+import AsideHeader from "./asideHeader/asideHeader";
+import CourierContent from "./courierContent/courierContent";
+import PickupContent from "./pickupContent/pickupContent";
+import YandexMap from "./yandexMap/yandexMap";
 
-import style from './address.module.sass';
-import PickupMap from './pickupMap/PickupMap';
-import PointContent from './pointContent/pointContent';
+import style from "./address.module.sass";
+import PickupMap from "./pickupMap/PickupMap";
+import PointContent from "./pointContent/pointContent";
 
 export interface IAddressState {
   address: {
@@ -30,7 +30,7 @@ interface IWorkTimeList {
   time: string;
 }
 
-interface IAddressObj {
+export interface IAddressObj {
   geometry: { coordinates: number[]; type: string };
   id: string;
   properties: {
@@ -38,16 +38,26 @@ interface IAddressObj {
     balloonContentFooter: string;
     balloonContentHeader: string;
   };
-  type: 'Feature';
+  type: "Feature";
   work_time_list: IWorkTimeList[];
 }
 
 const Address: FC = () => {
-  const isAddressOpen = useSelector((state: IAddressState) => state.address.isAddressOpen);
+  const isAddressOpen = useSelector(
+    (state: IAddressState) => state.address.isAddressOpen
+  );
   const type = useSelector((state: IType) => state.address.type);
   const dispatch = useDispatch();
   const router = useRouter();
   const [activeAddress, setActiveAddress] = useState<IAddressObj | null>(null);
+  const [mapCenter, setMapCenter] = useState<number[]>([43.33417, 45.68794]);
+  const [mapZoom, setMapZoom] = useState<number>(13);
+
+  const handleSetMapCenter = (coordinates: number[], zoom: number) => {
+    setMapCenter(coordinates);
+    setMapZoom(zoom);
+  };
+
   const closeAside = () => {
     dispatch(setIsAddressOpen(false));
   };
@@ -66,40 +76,58 @@ const Address: FC = () => {
         exit: style.slideExit,
         exitActive: style.slideExitActive,
       }}
-      unmountOnExit>
+      unmountOnExit
+    >
       <div>
         <div className={style.aside__wrap}>
-          <div className={style.empty + ' ' + (type === 'pickup' ? style.empty_small : '')}>
+          <div
+            className={
+              style.empty + " " + (type === "pickup" ? style.empty_small : "")
+            }
+          >
             <div className={style.closeBlock} onClick={closeAside}></div>
-            {type === 'point' && (
+            {type === "point" && (
               <div className={style.map}>
-                <YandexMap setActiveAddress={setActiveAddress} />
+                <YandexMap
+                  setActiveAddress={setActiveAddress}
+                  mapCenter={mapCenter}
+                  mapZoom={mapZoom}
+                />
               </div>
             )}
-            {type === 'pickup' && (
+            {type === "pickup" && (
               <div className={style.map}>
                 <PickupMap />
               </div>
             )}
           </div>
-          <div className={style.aside + ' ' + (type === 'pickup' ? style.aside_small : '')}>
+          <div
+            className={
+              style.aside + " " + (type === "pickup" ? style.aside_small : "")
+            }
+          >
             <AsideHeader
               title={
-                type === 'courier'
-                  ? 'АДРЕС ДОСТАВКИ'
-                  : type === 'point'
-                  ? 'выбор пункта выдачи'
-                  : type === 'pickup'
-                  ? 'магазин skincare agents'
-                  : ''
+                type === "courier"
+                  ? "АДРЕС ДОСТАВКИ"
+                  : type === "point"
+                  ? "выбор пункта выдачи"
+                  : type === "pickup"
+                  ? "магазин skincare agents"
+                  : ""
               }
               closeAside={closeAside}
             />
-            {type === 'courier' && <CourierContent closeAside={closeAside} />}
-            {type === 'point' && (
-              <PointContent closeAside={closeAside} activeAddress={activeAddress} />
+            {type === "courier" && <CourierContent closeAside={closeAside} />}
+            {type === "point" && (
+              <PointContent
+                closeAside={closeAside}
+                activeAddress={activeAddress}
+                setActiveAddress={setActiveAddress}
+                setMapCenter={handleSetMapCenter}
+              />
             )}
-            {type === 'pickup' && <PickupContent />}
+            {type === "pickup" && <PickupContent />}
           </div>
         </div>
       </div>
--- src/components/layout/address/pointContent/pointContent.tsx
+++ src/components/layout/address/pointContent/pointContent.tsx
@@ -1,8 +1,28 @@
-import { FC, useState } from 'react';
+import { FC, useEffect, useState } from "react";
 
-import Input from '@/components/other/input/input';
+import Input from "@/components/other/input/input";
 
-import style from './pointContent.module.sass';
+import style from "./pointContent.module.sass";
+import { useDispatch, useSelector } from "react-redux";
+import { IMapData } from "../yandexMap/yandexMap";
+import {
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+  Typography,
+  TextField,
+  Button,
+  Paper,
+  List,
+  ListItem,
+  ListItemText,
+} from "@mui/material";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import {
+  setAddress,
+  setMapData,
+  setSelectedCityCode,
+} from "@/redux/addressSlice/addressSlice";
 
 interface IWorkTimeList {
   day: number;
@@ -17,23 +37,107 @@ interface IAddressObj {
     balloonContentFooter: string;
     balloonContentHeader: string;
   };
-  type: 'Feature';
+  type: "Feature";
   work_time_list: IWorkTimeList[];
 }
 
 interface ICloseAside {
   closeAside: () => void;
   activeAddress: IAddressObj | null;
+  setActiveAddress: (address: IAddressObj) => void;
+  setMapCenter: (coordinates: number[], zoom: number) => void;
 }
 
-const PointContent: FC<ICloseAside> = ({ closeAside, activeAddress }) => {
-  const [city, setCity] = useState('');
-  const [street, setStreet] = useState('');
-  const [apartment, setApartment] = useState(''); //квартира
-  const [intercom, setIntercom] = useState(''); //домофон
-  const [entrance, setEntrance] = useState(''); //подъезд
-  const [floor, setFloor] = useState(''); //этаж
-  const days = ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'];
+const PointContent: FC<ICloseAside> = ({
+  closeAside,
+  activeAddress,
+  setActiveAddress,
+  setMapCenter,
+}) => {
+  const [city, setCity] = useState("");
+  // const [street, setStreet] = useState("S");
+  // const [apartment, setApartment] = useState("A"); //квартира
+  // const [intercom, setIntercom] = useState(""); //домофон
+  // const [entrance, setEntrance] = useState(""); //подъезд
+  // const [floor, setFloor] = useState(""); //этаж
+  const days = ["Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Вс"];
+
+  const dispatch = useDispatch();
+
+  const [expandedAddress, setExpandedAddress] = useState<string | false>(false);
+  const [filteredCities, setFilteredCities] = useState<
+    { city: string; code: number }[]
+  >([]);
+  const cities = useSelector((state: any) => state.address.cities);
+  const [debouncedCity, setDebouncedCity] = useState(city);
+  const [showSuggestions, setShowSuggestions] = useState(false);
+
+  const handleAccordionChange =
+    (address: IAddressObj) =>
+    (event: React.SyntheticEvent, isExpanded: boolean) => {
+      if (isExpanded) {
+        setActiveAddress(address);
+        setMapCenter(address.geometry.coordinates, 20);
+      }
+    };
+
+  const mapData: IMapData[] = useSelector(
+    (state: any) => state.address.mapData
+  );
+
+  console.log("mapData", filteredCities);
+  useEffect(() => {
+    if (activeAddress) {
+      setExpandedAddress(
+        `panel${mapData.findIndex((item) => item.id === activeAddress.id)}`
+      );
+    }
+  }, [activeAddress, mapData]);
+
+  const handleSaveAddress = (
+    e: React.MouseEvent<HTMLButtonElement>,
+    address: IAddressObj
+  ) => {
+    e.preventDefault();
+    dispatch(setAddress(address));
+    closeAside();
+  };
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedCity(city);
+    }, 500);
+
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [city]);
+
+  useEffect(() => {
+    if (debouncedCity) {
+      setFilteredCities(
+        cities.filter((c: any) =>
+          c.city.toLowerCase().includes(debouncedCity.toLowerCase())
+        )
+      );
+      setShowSuggestions(true);
+    } else {
+      setFilteredCities([]);
+      setShowSuggestions(false);
+    }
+  }, [debouncedCity, cities]);
+
+  const handleCitySelect = (
+    city: string,
+    code: number,
+    coordinates: number[]
+  ) => {
+    setCity(city);
+    dispatch(setSelectedCityCode(code));
+    setMapCenter(coordinates, 13);
+    setShowSuggestions(false);
+  };
+
   return (
     <form className={style.form}>
       <div className={style.content}>
@@ -45,28 +149,67 @@ const PointContent: FC<ICloseAside> = ({ closeAside, activeAddress }) => {
             isNecessary={true}
             onChange={(e) => setCity(e.target.value)}
           />
+          {showSuggestions && filteredCities.length > 0 && (
+            <Paper className={style.suggestions}>
+              <List>
+                {filteredCities.map((c: any) => (
+                  <ListItem
+                    style={{ cursor: "pointer" }}
+                    key={c.code}
+                    onClick={() =>
+                      handleCitySelect(c.city, c.code, [
+                        c.latitude,
+                        c.longitude,
+                      ])
+                    }
+                  >
+                    <ListItemText primary={c.city} />
+                  </ListItem>
+                ))}
+              </List>
+            </Paper>
+          )}
         </div>
       </div>
-      {activeAddress && (
-        <div className={style.middle}>
-          <div className={style.title}>СДЭК</div>
-          <div className={style.address}>{activeAddress.properties.balloonContentHeader}</div>
-          <div className={style.subtitle}>Режим работы</div>
-          <ul className={style.times}>
-            {activeAddress.work_time_list.map((item, i) => (
-              <li key={i}>
-                <span>{days[i] + ':'}</span> {item.time}
-              </li>
-            ))}
-          </ul>
-        </div>
-      )}
-
-      <button
-        className={style.btn + ' ' + (!(street && apartment && city) ? style.disabled : '')}
-        onClick={closeAside}>
-        сохранить адрес
-      </button>
+      {mapData.map((item, index) => (
+        <Accordion
+          key={item.id}
+          expanded={expandedAddress === `panel${index}`}
+          onChange={handleAccordionChange(item)}
+        >
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls={`panel${index}bh-content`}
+            id={`panel${index}bh-header`}
+          >
+            <Typography>{item.address}</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            {activeAddress && activeAddress.id === item.id && (
+              <div className={style.middle}>
+                <div className={style.title}>СДЭК</div>
+                <div className={style.address}>
+                  {activeAddress.properties.balloonContentHeader}
+                </div>
+                <div className={style.subtitle}>Режим работы</div>
+                <ul className={style.times}>
+                  {activeAddress.work_time_list.map((item, i) => (
+                    <li key={i}>
+                      <span>{days[i] + ":"}</span> {item.time}
+                    </li>
+                  ))}
+                </ul>
+                <button
+                  className={style.btn}
+                  onClick={(e) => handleSaveAddress(e, item)}
+                >
+                  выбрать
+                </button>
+              </div>
+            )}
+          </AccordionDetails>
+        </Accordion>
+      ))}
     </form>
   );
 };
