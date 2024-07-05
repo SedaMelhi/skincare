8df9a56f9baf2b818cccbeacd3afb69e9852cb9d
Rasul <overlord.ove@mail.ru>
2024-07-05 13:27:42 +0300

    упомянутые и карты в профиле

--- src/components/screens/maps/mapsPage.tsx
+++ src/components/screens/maps/mapsPage.tsx
@@ -1,60 +1,74 @@
-import { FC, useEffect, useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { setIsAddressOpen } from '@/redux/addressSlice/addressSlice';
+import { FC, useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { setIsAddressOpen, setType } from "@/redux/addressSlice/addressSlice";
 
-import Layout from '@/components/layout/Layout';
-import ProfileTitle from '../profile/profileTitle/Title';
-import ProfileAside from '../profile/profileAside/profileAside';
-import Footer from '@/components/layout/footer/footer';
-import Tab from '@/components/other/tab/tab';
-import InfoBlock from './infoBlock/infoBlock';
-import Button from '@/components/other/button/button';
-import PayCard from './payCard/payCard';
-import BasicPayCard from './basicPayCard/basicPayCard';
+import Layout from "@/components/layout/Layout";
+import ProfileTitle from "../profile/profileTitle/Title";
+import ProfileAside from "../profile/profileAside/profileAside";
+import Footer from "@/components/layout/footer/footer";
+import Tab from "@/components/other/tab/tab";
+import InfoBlock from "./infoBlock/infoBlock";
+import Button from "@/components/other/button/button";
+import PayCard from "./payCard/payCard";
+import BasicPayCard from "./basicPayCard/basicPayCard";
 
-import sbpPng from './../../../../public/certificate/сбп.png';
-import plusSvg from './../../../../public/certificate/plus.svg';
+import sbpPng from "./../../../../public/certificate/сбп.png";
+import plusSvg from "./../../../../public/certificate/plus.svg";
 
-import style from './maps.module.sass';
-import { API_URL } from '@/services';
-import { IUserData } from '../profile/profilePage';
-import { userInfoService } from '@/services/profile.service';
+import style from "./maps.module.sass";
+import { API_URL } from "@/services";
+import { IUserData } from "../profile/profilePage";
+import { userInfoService } from "@/services/profile.service";
 
 const MapsPage: FC = () => {
   const [activeEl, setActiveEl] = useState(0);
   const [activeTab, setActiveTab] = useState(1);
   const dispatch = useDispatch();
-  const [address, setAddress] = useState<{ id: number; addressDetail: any }[]>([]);
+  const [address, setAddress] = useState<{ id: number; addressDetail: any }[]>(
+    []
+  );
   const [userDataServer, setUserDataServer] = useState<IUserData>({
-    birthday: '',
-    email: '',
-    lastName: '',
-    loginPhone: '',
-    name: '',
-    secondName: '',
+    birthday: "",
+    email: "",
+    lastName: "",
+    loginPhone: "",
+    name: "",
+    secondName: "",
     userId: 0,
   });
   useEffect(() => {
     userInfoService.getUserInfo().then(setUserDataServer);
   }, []);
   const getNewAddress = () => {
-    fetch(API_URL + 'v1/user.php', {
-      method: 'POST',
-      body: JSON.stringify({ type: 'getAddress', token: localStorage.getItem('token') }),
+    fetch(API_URL + "v1/user.php", {
+      method: "POST",
+      body: JSON.stringify({
+        type: "getAddress",
+        token: localStorage.getItem("token"),
+      }),
     })
       .then((res) => res.json())
       .then((res) => {
-        setAddress(res['3']);
+        setAddress(res["3"]);
       });
   };
   useEffect(() => {
     getNewAddress();
   }, [activeTab]);
 
+  const handleAddAddress = () => {
+    if (activeTab === 0) {
+      dispatch(setType("point"));
+    } else {
+      dispatch(setType("courier"));
+    }
+    dispatch(setIsAddressOpen(true));
+  };
+
   return (
-    <Layout title={'Адреса'}>
+    <Layout title={"Адреса"}>
       <section className={style.wrap}>
-        <div className={'wrap ' + style.content}>
+        <div className={"wrap " + style.content}>
           <div className={style.aside}>
             <ProfileAside
               activeMenu={3}
@@ -69,34 +83,52 @@ const MapsPage: FC = () => {
             <ProfileTitle title="адреса" link={false} />
             <div className={style.tabs}>
               <div onClick={() => setActiveTab(0)}>
-                <Tab text="пункт выдачи" link="maps" active={activeTab === 0 ? true : false} />
+                <Tab
+                  text="пункт выдачи"
+                  link="maps"
+                  active={activeTab === 0 ? true : false}
+                />
               </div>
               <div onClick={() => setActiveTab(1)}>
-                <Tab text="курьером" link="maps" active={activeTab === 1 ? true : false} />
+                <Tab
+                  text="курьером"
+                  link="maps"
+                  active={activeTab === 1 ? true : false}
+                />
               </div>
             </div>
             {address &&
               address.map(({ id, addressDetail }) => (
                 <div className={style.address} key={id}>
                   <InfoBlock
-                    title={activeTab === 1 ? '' : 'почта россии'}
+                    title={activeTab === 1 ? "" : "почта россии"}
                     id={id}
                     getNewAddress={getNewAddress}
                     text={
-                      'г. ' +
+                      "г. " +
                       addressDetail.city +
-                      ', ' +
+                      ", " +
                       addressDetail.street +
-                      (addressDetail.entrance ? ', подъез: ' + addressDetail.entrance : '') +
-                      (addressDetail.floor ? ', этаж: ' + addressDetail.floor : '') +
-                      (addressDetail.intercom ? ', домофон: ' + addressDetail.intercom : '')
+                      (addressDetail.entrance
+                        ? ", подъезд: " + addressDetail.entrance
+                        : "") +
+                      (addressDetail.floor
+                        ? ", этаж: " + addressDetail.floor
+                        : "") +
+                      (addressDetail.intercom
+                        ? ", домофон: " + addressDetail.intercom
+                        : "")
                     }
                   />
                 </div>
               ))}
 
-            <div className={style.btn} onClick={() => dispatch(setIsAddressOpen(true))}>
-              <Button text="добавить новый адрес" height="44px" fontSize="14px" />
+            <div className={style.btn} onClick={handleAddAddress}>
+              <Button
+                text="добавить новый адрес"
+                height="44px"
+                fontSize="14px"
+              />
             </div>
             <div className={style.pay}>
               <ProfileTitle title="способ оплаты" link={false} />
--- src/components/screens/placing/placingPage.tsx
+++ src/components/screens/placing/placingPage.tsx
@@ -1,31 +1,36 @@
-import Layout from '@/components/layout/Layout';
-import { IbasketData } from '@/interfaces/basket.interface';
-import { IOrder } from '@/interfaces/order.interface';
-import { API_URL } from '@/services';
-import { FC, useEffect, useState } from 'react';
-import { useRouter } from 'next/router';
-import Link from 'next/link';
-import { getBasketService, getCertificate, getOrderInfoService } from '@/services/order.service';
-import { setIsAddressOpen } from '@/redux/addressSlice/addressSlice';
-import { useDispatch, useSelector } from 'react-redux';
+import Layout from "@/components/layout/Layout";
+import { IbasketData } from "@/interfaces/basket.interface";
+import { IOrder } from "@/interfaces/order.interface";
+import { API_URL } from "@/services";
+import { FC, useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import Link from "next/link";
+import {
+  getBasketService,
+  getCertificate,
+  getOrderInfoService,
+} from "@/services/order.service";
+import { setIsAddressOpen } from "@/redux/addressSlice/addressSlice";
+import { useDispatch, useSelector } from "react-redux";
 
-import BasketRight from './basketRight/basketRight';
-import RangeSlider from './rangeSlider/rangeSlider';
-import Methods from './methods/methods';
-import arrowLeft from './../../../../public/arrow.svg';
-import close from './../../../../public/close.svg';
-import whiteArrow from './../../../../public/whiteArrow.svg';
-import plusSvg from './../../../../public/plusSimple.svg';
-import arrowViolet from './../../../../public/arrowViolet.svg';
+import BasketRight from "./basketRight/basketRight";
+import RangeSlider from "./rangeSlider/rangeSlider";
+import Methods from "./methods/methods";
+import arrowLeft from "./../../../../public/arrow.svg";
+import close from "./../../../../public/close.svg";
+import whiteArrow from "./../../../../public/whiteArrow.svg";
+import plusSvg from "./../../../../public/plusSimple.svg";
+import arrowViolet from "./../../../../public/arrowViolet.svg";
 
-import style from './placing.module.sass';
+import style from "./placing.module.sass";
 
 const PlacingPage: FC = () => {
   const router = useRouter();
   const dispatch = useDispatch();
   const address = useSelector((state: any) => state.address.isAddressOpen);
   const [basket, setBasket] = useState<IOrder>();
-  const savedAddress = useSelector((state: any) => state.address.address);
+  // const savedAddress = useSelector((state: any) => state.address.address);
+  const [savedAddress, setSaveAddress] = useState<any>();
   const [isAuth, setIsAuth] = useState<any>();
   const [certificate, setCertificate] = useState<any>();
   const sendCertificate = async (e: any) => {
@@ -33,24 +38,26 @@ const PlacingPage: FC = () => {
     const res = await getCertificate.getBasket();
   };
   useEffect(() => {
-    const basket = getBasketService.getBasket(localStorage.getItem('saleUserId'));
+    const basket = getBasketService.getBasket(
+      localStorage.getItem("saleUserId")
+    );
     basket.then((res: IOrder) => setBasket(res));
-    fetch(API_URL + 'v1/user.php', {
-      method: 'POST',
+    fetch(API_URL + "v1/user.php", {
+      method: "POST",
       body: JSON.stringify({
-        type: 'getAddress',
-        token: localStorage.getItem('token'),
+        type: "getAddress",
+        token: localStorage.getItem("token"),
       }),
     })
       .then((res) => res.json())
       .then((res) => {
-        if (res['3']) {
-          // setSaveAddress(res['3'][res['3'].length - 1]);
+        if (res["3"]) {
+          setSaveAddress(res["3"][res["3"].length - 1]);
         }
       });
   }, [address]);
   useEffect(() => {
-    setIsAuth(localStorage.getItem('token'));
+    setIsAuth(localStorage.getItem("token"));
   }, []);
   const changeAddress = () => {};
 
@@ -73,49 +80,26 @@ const PlacingPage: FC = () => {
             </div>
             <div className={style.subtitle}>Доставка</div>
             <Methods />
-            <div className={style.method + ' ' + style.flex_start}>
+            <div className={style.method + " " + style.flex_start}>
               <div className={style.method__title}>адрес доставки</div>
               <div className={style.box}>
                 <div className={style.address}>
-                  {savedAddress.city.name && (
-                    <div>Город: {savedAddress.city.name}</div>
-                  )}
-                  {savedAddress.street.name && (
-                    <div>Улица: {savedAddress.street.name}</div>
-                  )}
-                  {savedAddress.apartment && (
-                    <div>Квартира: {savedAddress.apartment}</div>
-                  )}
-                  {savedAddress.intercom && (
-                    <div>Домофон: {savedAddress.intercom}</div>
-                  )}
-                  {savedAddress.entrance && (
-                    <div>Подъезд: {savedAddress.entrance}</div>
-                  )}
-                  {savedAddress.floor && <div>Этаж: {savedAddress.floor}</div>}
-                  {savedAddress.full_address && (
-                    <div>Полный адрес: {savedAddress.full_address}</div>
-                  )}
-                  {!savedAddress.city.name &&
-                    !savedAddress.street.name &&
-                    !savedAddress.apartment &&
-                    !savedAddress.intercom &&
-                    !savedAddress.entrance &&
-                    !savedAddress.floor &&
-                    !savedAddress.full_address &&
-                    "не указан"}
+                  {savedAddress && savedAddress.addressDetail.city
+                    ? `г. ${savedAddress.addressDetail.city}, ${savedAddress.addressDetail.street}, д ${savedAddress.addressDetail.entrance}, кв./офис ${savedAddress.addressDetail.apartment}`
+                    : "не указан"}
                 </div>
                 <button
                   className={style.btn}
                   onClick={() => {
                     dispatch(setIsAddressOpen(true));
                     changeAddress();
-                  }}>
+                  }}
+                >
                   изменить адрес
                 </button>
               </div>
             </div>
-            <Link href={'/recipient'} className={style.button}>
+            <Link href={"/recipient"} className={style.button}>
               продолжить
               <div>
                 <img src={whiteArrow.src} alt="" />
@@ -124,10 +108,11 @@ const PlacingPage: FC = () => {
           </div>
           <div className={style.right}>
             {!isAuth && (
-              <Link href={'/authorization'}>
-                <div className={style.message + ' ' + style.pc}>
+              <Link href={"/authorization"}>
+                <div className={style.message + " " + style.pc}>
                   <div className={style.message__text}>
-                    Зарегистрируйтесь/войдите, чтобы получать кэшбек со своих покупок
+                    Зарегистрируйтесь/войдите, чтобы получать кэшбек со своих
+                    покупок
                   </div>
                   <div>
                     <img src={arrowViolet.src} alt="" />
@@ -137,12 +122,18 @@ const PlacingPage: FC = () => {
             )}
 
             {basket && <BasketRight basket={basket} />}
-            <div className={style.message__text + ' ' + style.mobile}>
-              Зарегистрируйтесь/войдите, чтобы получать кэшбек со своих покупок и применять
-              сертификат.
+            <div className={style.message__text + " " + style.mobile}>
+              Зарегистрируйтесь/войдите, чтобы получать кэшбек со своих покупок
+              и применять сертификат.
             </div>
-            <div className={style.promocode__wrap + ' ' + style.promocode__first}>
-              <input type="text" className={style.promocode} placeholder="Промокод" />
+            <div
+              className={style.promocode__wrap + " " + style.promocode__first}
+            >
+              <input
+                type="text"
+                className={style.promocode}
+                placeholder="Промокод"
+              />
               <img src={plusSvg.src} alt="" />
             </div>
             <div className={style.promocode__wrap}>
