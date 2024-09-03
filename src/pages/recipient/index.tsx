ebfe66168e60db1d6a0291c7c9133d77c25f1a00
SedaMelkhi <31256499+SedaMelhi@users.noreply.github.com>
2024-09-03 14:27:44 +0300

    fix: reception

--- src/pages/recipient/index.tsx
+++
@@ -1,8 +0,0 @@
-import { NextPage } from 'next';
-import RecipientPage from '@/components/screens/recipient/RecipientPage';
-
-const Recipient: NextPage = ({}) => {
-  return <RecipientPage />;
-};
-
-export default Recipient;
