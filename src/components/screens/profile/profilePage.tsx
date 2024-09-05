29663529efdbf33af0d9add8f2700f6f874d3a8b
SedaMelkhi <31256499+SedaMelhi@users.noreply.github.com>
2024-09-05 13:43:54 +0300

    fix: user order

--- src/components/screens/profile/profilePage.tsx
+++ src/components/screens/profile/profilePage.tsx
@@ -1,12 +1,12 @@
-import { FC, useEffect, useState } from 'react';
-import { userInfoService } from '@/services/profile.service';
+import { FC, useEffect, useState } from "react";
+import { userInfoService } from "@/services/profile.service";
 
-import Layout from '@/components/layout/Layout';
+import Layout from "@/components/layout/Layout";
 
-import ProfileAside from './profileAside/profileAside';
-import ProfileData from './profileData/profileData';
+import ProfileAside from "./profileAside/profileAside";
+import ProfileData from "./profileData/profileData";
 
-import style from './profile.module.sass';
+import style from "./profile.module.sass";
 
 export interface IUserData {
   birthday: string;
@@ -21,23 +21,24 @@ export interface IUserData {
 const ProfilePage: FC = () => {
   const [activeProfileData, setActiveProfileData] = useState(true);
   const [userData, setUserData] = useState<IUserData>({
-    birthday: '',
-    email: '',
-    lastName: '',
-    loginPhone: '',
-    name: '',
-    secondName: '',
+    birthday: "",
+    email: "",
+    lastName: "",
+    loginPhone: "",
+    name: "",
+    secondName: "",
     userId: 0,
   });
   const [userDataServer, setUserDataServer] = useState<IUserData>({
-    birthday: '',
-    email: '',
-    lastName: '',
-    loginPhone: '',
-    name: '',
-    secondName: '',
+    birthday: "",
+    email: "",
+    lastName: "",
+    loginPhone: "",
+    name: "",
+    secondName: "",
     userId: 0,
   });
+
   useEffect(() => {
     userInfoService.getUserInfo().then(setUserData);
   }, []);
@@ -48,14 +49,14 @@ const ProfilePage: FC = () => {
   return (
     <Layout title="Личный кабинет">
       <div className={style.profile__wrap}>
-        <div className={'wrap ' + style.profile}>
-          <div className={!activeProfileData ? style.hidden : ''}>
+        <div className={"wrap " + style.profile}>
+          <div className={!activeProfileData ? style.hidden : ""}>
             <ProfileAside
               setActiveProfileData={setActiveProfileData}
               userDataServer={userDataServer}
             />
           </div>
-          <div className={activeProfileData ? style.hidden : ''}>
+          <div className={activeProfileData ? style.hidden : ""}>
             <ProfileData
               setActiveProfileData={setActiveProfileData}
               userDataServer={userDataServer}
