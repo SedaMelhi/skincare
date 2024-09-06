8661563b9821e5217fdcf4fecda1f0cd5f074bbb
SedaMelkhi <31256499+SedaMelhi@users.noreply.github.com>
2024-09-06 22:49:51 +0300

    fix: catalog filter style

--- src/components/other/filters/filter/filter.tsx
+++ src/components/other/filters/filter/filter.tsx
@@ -1,22 +1,25 @@
-import { FC, useEffect, useState } from 'react';
-import { setCheckboxFilters } from '@/redux/catalogSlice/catalogSlice';
-import minusSvg from './../../../../../public/catalog/minus.svg';
-import { Accordion, AccordionDetails, AccordionSummary } from '@mui/material';
-import { useDispatch, useSelector } from 'react-redux';
+import { FC, useEffect, useState } from "react";
+import { setCheckboxFilters } from "@/redux/catalogSlice/catalogSlice";
+import minusSvg from "./../../../../../public/catalog/minus.svg";
+import { Accordion, AccordionDetails, AccordionSummary } from "@mui/material";
+import { useDispatch, useSelector } from "react-redux";
 
-import style from './filter.module.sass';
-
+import style from "./filter.module.sass";
 
 const Filter: FC<{
   name: any;
   items: { name: string; id: string }[];
   id: string;
 }> = ({ name, items, id }) => {
-  const [renderItems, setRenderItems] = useState(items.filter((item, i) => i < 3));
-  const [isShow, setIsShow] = useState(false)
-  const [isOpen, setIsOpen] = useState(false)
+  const [renderItems, setRenderItems] = useState(
+    items.filter((item, i) => i < 3)
+  );
+  const [isShow, setIsShow] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
   const [firstLoad, setFirstLoad] = useState(true);
-  const checkboxFilters = useSelector((state: any) => state.catalog.checkboxFilters);
+  const checkboxFilters = useSelector(
+    (state: any) => state.catalog.checkboxFilters
+  );
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -26,52 +29,54 @@ const Filter: FC<{
   const addCheckboxFilter = (i: string) => {
     if (checkboxFilters[id]) {
       let params = checkboxFilters[id].some((item: any) => item === i)
-        ? { ...checkboxFilters, [id]: checkboxFilters[id].filter((item: any) => item !== i) }
-        : { ...checkboxFilters, [id]: [...checkboxFilters[id], i] }
+        ? {
+            ...checkboxFilters,
+            [id]: checkboxFilters[id].filter((item: any) => item !== i),
+          }
+        : { ...checkboxFilters, [id]: [...checkboxFilters[id], i] };
       if (params[id].length === 0) {
-        delete params[id]
+        delete params[id];
       }
-      dispatch(
-        setCheckboxFilters(
-          params
-        ),
-      );
+      dispatch(setCheckboxFilters(params));
     } else {
       dispatch(setCheckboxFilters({ ...checkboxFilters, [id]: [i] }));
     }
   };
   return (
-    <div className={firstLoad ? style.load : ''}>
-      <Accordion
-        className={style.accordion}
-      >
+    <div className={"catalogFilter " + (firstLoad ? style.load : "")}>
+      <Accordion className={"accordion"}>
         <AccordionSummary
-          className={style.params__wrap}
+          className={"params__wrap"}
           onClick={() => {
-            setRenderItems(items.filter((item, i) => i < 3))
-            setIsShow(false)
-            setIsOpen(prev => !prev)
-          }}>
+            setRenderItems(items.filter((item, i) => i < 3));
+            setIsShow(false);
+            setIsOpen((prev) => !prev);
+          }}
+        >
           <div className={style.title}>
             <span>{name} </span>
             <div className={style.sign}>
-              <img src={minusSvg.src} alt="" style={isOpen ? { opacity: '0' } : {}} />
+              <img
+                src={minusSvg.src}
+                alt=""
+                style={isOpen ? { opacity: "0" } : {}}
+              />
               <img src={minusSvg.src} alt="" />
             </div>
           </div>
         </AccordionSummary>
-        <AccordionDetails className={style.params}>
+        <AccordionDetails className="params">
           {renderItems.map((item, i) => (
-            <label
-              className={style.item}
-              key={item.id}>
+            <label className={style.item} key={item.id}>
               <input
                 type="checkbox"
                 name={item.id}
                 value={item.name}
                 checked={
                   checkboxFilters[id]
-                    ? checkboxFilters[id].some((filter: any) => filter === item.id)
+                    ? checkboxFilters[id].some(
+                        (filter: any) => filter === item.id
+                      )
                     : false
                 }
                 onChange={() => addCheckboxFilter(item.id)}
@@ -80,21 +85,24 @@ const Filter: FC<{
               <div className={style.input__text}>{item.name}</div>
             </label>
           ))}
-          <div className={style.renderItems} onClick={() => {
-            if (!isShow) {
-              setRenderItems(items)
-            } else {
-              setRenderItems(items.filter((item, i) => i < 3))
-            }
+          <div
+            className={style.renderItems}
+            onClick={() => {
+              if (!isShow) {
+                setRenderItems(items);
+              } else {
+                setRenderItems(items.filter((item, i) => i < 3));
+              }
 
-            setIsShow(prev => !prev)
-          }}>
+              setIsShow((prev) => !prev);
+            }}
+          >
             <div className={style.line}></div>
-            <span>{!isShow ? 'Показать все' : 'Скрыть'}</span>
+            <span>{!isShow ? "Показать все" : "Скрыть"}</span>
           </div>
         </AccordionDetails>
       </Accordion>
-    </div >
+    </div>
   );
 };
 
