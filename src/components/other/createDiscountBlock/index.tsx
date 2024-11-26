e2279b8cb730b858cfc5d698f9bf2c393426f5ef
rnazirov <rnazirov@marpla.ru>
2024-11-26 15:00:11 +0300

    привязал функционал для отпраки промокода (пока в консоли)

--- src/components/other/createDiscountBlock/index.tsx
+++ src/components/other/createDiscountBlock/index.tsx
@@ -1,23 +1,30 @@
 import React, { FC, useState } from "react";
 import style from "./createDiscountBlock.module.sass";
 import plusSvg from "./../../../../public/plusSimple.svg";
-import { getCertificate } from "@/services/order.service";
+import { getCertificate, getPromoCode } from "@/services/order.service";
 
 interface CreateDiscountBlockProps {
   isAuth: boolean;
 }
 
 const CreateDiscountBlock: FC<CreateDiscountBlockProps> = ({ isAuth }) => {
-  const [certificate, setCertificate] = useState<any>();
+  const [certificate, setCertificate] = useState<string>("");
+  const [promoCode, setPromoCode] = useState<string>("");
+
 
   const sendCertificate = async () => {
     await getCertificate.createDiscount(certificate);
   };
+
+  const sendPromoCode = async () => {
+    await getPromoCode.createDiscount(promoCode);
+  };
+
   return (
     <div>
       <div className={style.promocode__wrap + " " + style.promocode__first}>
-        <input type="text" className={style.promocode} placeholder="Промокод" />
-        <img src={plusSvg.src} alt="" />
+        <input value={promoCode} onChange={e => setPromoCode(e.target.value)} type="text" className={style.promocode} placeholder="Промокод" />
+        <img onClick={sendPromoCode} src={plusSvg.src} alt="" />
       </div>
       {isAuth && (
         <div className={style.promocode__wrap}>
